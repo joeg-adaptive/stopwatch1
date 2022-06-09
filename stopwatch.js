ebb5ad829c5721a2cf389ce29172d4cbb4c96150
Joe Goolic <joegoolic@Joes-MBP.si.rr.com>
2022-06-09 14:41:20 -0400

    feat using Date.now instead of incrementing microseconds for increased accuracy

--- stopwatch.js
+++ stopwatch.js
@@ -1,41 +1,34 @@
-
-let interval;
-let microseconds = 0;
-let seconds = 0;
-let minutes = 0;
-let whichLap = 0;
-let lastConvertedtime = 0;
-let previousConvertedtime = 0;
-let currentConvertedtime = 0;
 let displayTime = 0;
-let currentMicrosecond = 0;
 let smallest = 0;
 let largest = 0;
-let convertedBackminutes = 0;
-let convertedBackseconds = 0;
-let convertedBackmicroseconds= 0;
 let time;
+let interval;
+let whichLap = 0;
+let lastLapTime = 0;
+let newLaptime = 0;
+let convertedPreviousTimeObj = { minutes:0, seconds:0, centiseconds:0 }
+let rawPreviousTimeObj = { centiseconds:0 }
+let convertedCurrentTimeObj = { minutes:0, seconds:0, centiseconds:0 }
+let rawCurrentTimeObj = { centiseconds:0 }
 
-//timeCounter for using setInterval to 
+//Function to count the time
 function timeCounter(){
+    let startTime=Date.now()
     interval = setInterval(function(){
-        microseconds ++
-        if (microseconds == 100){
-            microseconds = 0
-            seconds ++
-        } 
-        if(seconds == 60 ){
-            seconds = 0
-            minutes++
-        }
-        document.getElementById("timer").innerHTML = Formatter(minutes,seconds,microseconds)
+        let currentTime = Date.now();
+        rawCurrentTimeObj.centiseconds = currentTime - startTime;
+        //console.log(rawCurrentTimeObj.centiseconds)
+        convertBack(rawCurrentTimeObj.centiseconds)
+        document.getElementById("timer").innerHTML = Formatter(convertedCurrentTimeObj.minutes,convertedCurrentTimeObj.seconds,convertedCurrentTimeObj.centiseconds)
     },10)
 }
 
+//Handle Clearing interval
 function stopTimer(){
     clearInterval(interval)
 }
 
+//Handle startStop Button Function
 function startStop(){
     startStopButton = document.getElementById('startStop')
     startStoptext = startStopButton.innerText
@@ -53,6 +46,8 @@ function startStop(){
         timeCounter();
     } 
 }
+
+//Handle resetLap Button Function
 function resetLap(){
     resetLapButton = document.getElementById('resetLap')
     resetLaptext = resetLapButton.innerText
@@ -60,7 +55,7 @@ function resetLap(){
         resetValues()
     }
     else if(resetLaptext === "Lap"){
-        lapTracker(minutes,seconds,microseconds)
+        lapTracker()
     } 
 }
 
@@ -81,21 +76,21 @@ function resetValues(){
 }
 
 //this function is responsible for adding the html with the laps time and keeping track of the whichLap variable
-// It calls "lastValueCalc" to begin calculations got laps
-function lapTracker(minutes,seconds,microseconds){
-    if(whichLap ==0){
-        lastValueCalc(minutes,seconds,microseconds)
+// It calls "lapCalc" to begin calculations got laps
+function lapTracker(){
+    if(whichLap == 0){
+        lapCalc(rawCurrentTimeObj.centiseconds)
         var node = document.createElement('li');
         node.setAttribute('id',whichLap)
         document.querySelector('ul').appendChild(node);
-        node.appendChild(document.createTextNode(`Lap ${whichLap + 1}   `+ lastValueCalc(minutes,seconds,microseconds)));
+        node.appendChild(document.createTextNode(`Lap ${whichLap + 1}   `+ lapCalc(rawCurrentTimeObj.centiseconds)));
         document.querySelector('ul').appendChild(node);
         node.appendChild(document.createElement('hr'));
         whichLap++
     }else{
     var node = document.createElement('li');
     node.setAttribute('id',whichLap)
-    node.appendChild(document.createTextNode(`Lap ${whichLap + 1}    `+ lastValueCalc(minutes,seconds,microseconds)));
+    node.appendChild(document.createTextNode(`Lap ${whichLap + 1}    `+ lapCalc(rawCurrentTimeObj.centiseconds)));
     old_id = whichLap - 1
     let old = document.getElementById(old_id)
     console.log(old_id)
@@ -107,21 +102,22 @@ function lapTracker(minutes,seconds,microseconds){
 
 //Function to Save last value of time (in centiseconds) and subtract to newest time in centiseconds to get elapsed laptime
 //This function also converts and replaces the "old" time with the new time after conversion
-function lastValueCalc(minutes,seconds,microseconds){
+function lapCalc(){
     if(whichLap == 0){
-        lastConvertedtime = conversion(minutes,seconds,microseconds)
-        displayTime = lastConvertedtime
+        lastLapTime = rawCurrentTimeObj.centiseconds
+        displayTime = lastLapTime
     }else{
-        newConvertedtime = conversion(minutes,seconds,microseconds)
-        displayTime = (newConvertedtime - lastConvertedtime)
-        lastConvertedtime = newConvertedtime
+        newLaptime = rawCurrentTimeObj.centiseconds
+        displayTime = (newLaptime - lastLapTime)
+        lastLapTime = newLaptime
     }
     console.log(displayTime)
     convertedBackdisplayTime = convertBack(displayTime)
-    time = LapFormatter(convertedBackminutes,convertedBackseconds,convertedBackmicroseconds)
+    time = LapFormatter(convertedCurrentTimeObj.minutes,convertedCurrentTimeObj.seconds,convertedCurrentTimeObj.centiseconds)
     console.log(time)
     return(time)
 }
+
 //Function to convert all values down to centiseconds
 function conversion(minutes,seconds,microseconds){
     let minutesToSeconds = minutes * 60;
@@ -129,27 +125,30 @@ function conversion(minutes,seconds,microseconds){
     let convertedMicroseconds = secondsToMicroseconds + microseconds;
     return convertedMicroseconds
 }
+
 //Function to convert back to minutes and seconds and centiseconds
 function convertBack(convertedTime){
-    convertedBackminutes = Math.floor(convertedTime / 6000)
-        convertedTime -= convertedBackminutes * 6000;
-    convertedBackseconds = Math.floor(convertedTime / 100);
-        convertedTime -= convertedBackseconds * 100;
-    convertedBackmicroseconds = convertedTime;
+    convertedCurrentTimeObj.minutes = Math.floor(convertedTime / 60000)
+        convertedTime -= convertedCurrentTimeObj.minutes * 60000;
+    convertedCurrentTimeObj.seconds = Math.floor(convertedTime / 1000);
+        convertedTime -= convertedCurrentTimeObj.seconds * 1000;
+    convertedCurrentTimeObj.centiseconds = Math.floor((convertedTime)/10);
 }
+
 //Function to Format Stopwatch time
-function Formatter(){
+function Formatter(minutes,seconds,microseconds){
     let formattedMicroseconds = microseconds.toString().padStart(2,"0")
     let formattedSeconds = seconds.toString().padStart(2,"0")
     let formattedMinutes = minutes.toString().padStart(2,"0")
 
 return(`${formattedMinutes}:${formattedSeconds}.${formattedMicroseconds}`)
 }
+
 //Function to Format Laps 
-function LapFormatter(){
-    let formattedMicroseconds = convertedBackmicroseconds.toString().padStart(2,"0")
-    let formattedSeconds = convertedBackseconds.toString().padStart(2,"0")
-    let formattedMinutes = convertedBackminutes.toString().padStart(2,"0")
+function LapFormatter(minutes,seconds,microseconds){
+    let formattedMicroseconds = microseconds.toString().padStart(2,"0")
+    let formattedSeconds = seconds.toString().padStart(2,"0")
+    let formattedMinutes = minutes.toString().padStart(2,"0")
 
 return(`${formattedMinutes}:${formattedSeconds}.${formattedMicroseconds}`)
 }
