bc996bc620d1f4fb1595ccb1ace817be19b61a76
Joe Goolic <joegoolic@Joes-MBP.si.rr.com>
2022-06-09 08:48:37 -0400

    working stopwatch with laps

--- stopwatch.js
+++ stopwatch.js
@@ -3,7 +3,18 @@ let interval;
 let microseconds = 0;
 let seconds = 0;
 let minutes = 0;
-let laps = [];
+let whichLap = 0;
+let lastConvertedtime = 0;
+let previousConvertedtime = 0;
+let currentConvertedtime = 0;
+let displayTime = 0;
+let currentMicrosecond = 0;
+let smallest = 0;
+let largest = 0;
+let convertedBackminutes = 0;
+let convertedBackseconds = 0;
+let convertedBackmicroseconds= 0;
+let time;
 
 function timeCounter(){
     interval = setInterval(function(){
@@ -16,22 +27,7 @@ function timeCounter(){
             seconds = 0
             minutes++
         }
-        if(microseconds === 0){
-            document.getElementById("timer").innerHTML = `${0}${minutes}:${0}${seconds}.${00}`
-        }
-        else if(microseconds < 10){
-            document.getElementById("timer").innerHTML = `${minutes}:${seconds}.${0}${microseconds}`
-        }
-        else if(seconds < 10){
-            document.getElementById("timer").innerHTML = `${0}${minutes}:${0}${seconds}.${microseconds}`
-        }
-        else if(minutes < 10){
-            document.getElementById("timer").innerHTML = `${0}${minutes}:${0}${seconds}.${microseconds}`
-
-        }
-        else{
-            document.getElementById("timer").innerHTML = `${minutes}:${seconds}.${microseconds}`
-        }
+        document.getElementById("timer").innerHTML = Formatter(minutes,seconds,microseconds)
     },10)
 }
 
@@ -63,30 +59,87 @@ function resetLap(){
         resetValues()
     }
     else if(resetLaptext === "Lap"){
-        lapTracker()
-        let TimeObject = {minutes, seconds, microseconds};
-        laps.push(TimeObject)
-        console.log(laps)
+        lapTracker(minutes,seconds,microseconds)
     } 
 }
+
 function resetValues(){
     minutes = 0;
     seconds = 0;
     microseconds = 0;
+    laps = []
+    let current_list = document.querySelectorAll("li")
+    console.log(current_list)
+    for (let i = 0; (li = current_list[i]); i++) {
+        li.parentNode.removeChild(li);
+    }
+    whichLap = 0 
     document.getElementById("timer").innerHTML = `${0}${minutes}:${0}${seconds}.${0}${microseconds}`
 }
-function lapTracker(){
-    console.log('before if' + laps.length)
-    if (laps.length >= 0){
-        console.log('after if' + laps.length)
-        for(let i = laps.length-1; i--; i == 0){
-            console.log('value of i ' + i)
-            var node = document.createElement('li');
-            //node.appendChild(document.createTextNode(laps[i].minutes));
-            //node.appendChild(document.createTextNode(laps.map(laps => laps.minutes)));
-            //node.appendChild(document.createTextNode(laps[minutes][seconds][microseconds]));
-            node.appendChild(document.createTextNode(Object.values(laps[i])));
-            document.querySelector('ul').appendChild(node);
-        }
+
+function lapTracker(minutes,seconds,microseconds){
+    if(whichLap ==0){
+        lastValueCalc(minutes,seconds,microseconds)
+        var node = document.createElement('li');
+        node.setAttribute('id',whichLap)
+        document.querySelector('ul').appendChild(node);
+        node.appendChild(document.createTextNode(`Lap ${whichLap + 1}   `+ lastValueCalc(minutes,seconds,microseconds)));
+        document.querySelector('ul').appendChild(node);
+        node.appendChild(document.createElement('hr'));
+        whichLap++
+    }else{
+    var node = document.createElement('li');
+    node.setAttribute('id',whichLap)
+    node.appendChild(document.createTextNode(`Lap ${whichLap + 1}    `+ lastValueCalc(minutes,seconds,microseconds)));
+    old_id = whichLap - 1
+    let old = document.getElementById(old_id)
+    console.log(old_id)
+    old.insertAdjacentElement('beforebegin',node)
+    node.appendChild(document.createElement('hr'));
+    whichLap++
+    
+    
     }
+}
+function lastValueCalc(minutes,seconds,microseconds){
+    if(whichLap == 0){
+        lastConvertedtime = conversion(minutes,seconds,microseconds)
+        displayTime = lastConvertedtime
+    }else{
+        newConvertedtime = conversion(minutes,seconds,microseconds)
+        displayTime = (newConvertedtime - lastConvertedtime)
+        lastConvertedtime = newConvertedtime
+    }
+    console.log(displayTime)
+    convertedBackdisplayTime = convertBack(displayTime)
+    time = LapFormatter(convertedBackminutes,convertedBackseconds,convertedBackmicroseconds)
+    console.log(time)
+    return(time)
+}
+function conversion(minutes,seconds,microseconds){
+    let minutesToSeconds = minutes * 60;
+    let secondsToMicroseconds = (seconds + minutesToSeconds) * 100;
+    let convertedMicroseconds = secondsToMicroseconds + microseconds;
+    return convertedMicroseconds
+}
+function convertBack(convertedTime){
+    convertedBackminutes = Math.floor(convertedTime / 6000)
+        convertedTime -= convertedBackminutes * 6000;
+    convertedBackseconds = Math.floor(convertedTime / 100);
+        convertedTime -= convertedBackseconds * 100;
+    convertedBackmicroseconds = convertedTime;
+}
+function Formatter(){
+    let formattedMicroseconds = microseconds.toString().padStart(2,"0")
+    let formattedSeconds = seconds.toString().padStart(2,"0")
+    let formattedMinutes =minutes.toString().padStart(2,"0")
+
+return(`${formattedMinutes}:${formattedSeconds}.${formattedMicroseconds}`)
+}
+function LapFormatter(){
+    let formattedMicroseconds = convertedBackmicroseconds.toString().padStart(2,"0")
+    let formattedSeconds = convertedBackseconds.toString().padStart(2,"0")
+    let formattedMinutes = convertedBackminutes.toString().padStart(2,"0")
+
+return(`${formattedMinutes}:${formattedSeconds}.${formattedMicroseconds}`)
 }
