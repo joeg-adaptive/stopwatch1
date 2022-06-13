36eec5d26d0d1cff05f2925b29a1c3d78fb41795
Joe Goolic <joegoolic@Joes-MBP-Work.si.rr.com>
2022-06-13 09:10:10 -0400

    feat- added live counter on the first lap so it is accurate to the laptime

--- stopwatch.js
+++ stopwatch.js
@@ -1,233 +1,264 @@
-
-let time;
 let interval;
-let fastest = 0;
-let fastestLap = 0;
-let slowest = 0;
-let slowestLap = 0;
+let liveTime = false;
 let lastLapTime = 0;
-let newLaptime = 0;
 let whichLap = 0;
-let displayTime = 0;
-let convertedPreviousTimeObj = { minutes:0, seconds:0, centiseconds:0 }
-let rawPreviousTimeObj = { centiseconds:0 }
-let convertedCurrentTimeObj = { minutes:0, seconds:0, centiseconds:0 }
-let rawCurrentTimeObj = { centiseconds:0 }
-let rawPausedTimeObj = { centiseconds:0 }
+let fastestLapElementAndTime = { fastestTime: 0, fastestLap: 0 };
+let slowestLapElementAndTime = { slowestTime: 0, slowestLap: 0 };
+let formattedCurrentTime = { minutes: 0, seconds: 0, centiseconds: 0 };
+let liveLapTime = { minutes: 0, seconds: 0, centiseconds: 0 };
+let formattedLiveLapTime = { minutes: 0, seconds: 0, centiseconds: 0 };
+let unformattedCurrentTime = { centiseconds: 0 };
+let unformattedPausedTime = { centiseconds: 0 };
+
 //Feature List
-//Track fastest and slowest lap
 //Make latest lap realtime
-//Fix bug with time not starting where it left off when paused
 
 //Function to count the time
-function timeCounter(){
-    let startTime=Date.now()
-    interval = setInterval(function(){
+const timeCounter = () => {
+    let startTime = Date.now();
+    interval = setInterval(() => {
         let currentTime = Date.now();
-        rawCurrentTimeObj.centiseconds = currentTime - startTime;
-        rawCurrentTimeObj.centiseconds += rawPausedTimeObj.centiseconds
-        convertBack(rawCurrentTimeObj.centiseconds)
-        document.getElementById("timer").innerHTML = Formatter(convertedCurrentTimeObj.minutes,convertedCurrentTimeObj.seconds,convertedCurrentTimeObj.centiseconds)
-    },10)
-}
+        unformattedCurrentTime.centiseconds = currentTime - startTime;
+        unformattedCurrentTime.centiseconds +=
+            unformattedPausedTime.centiseconds;
+        formatTime(unformattedCurrentTime.centiseconds);
+        getLiveLapTime(unformattedCurrentTime.centiseconds);
+        document.getElementById("timer").innerText = getFormattedTime();
+        document.querySelector(".tdTime").innerText = getFormattedLiveLapTime();
+    }, 10);
+};
 
 //Handle Clearing interval
-function stopTimer(){
-    rawPausedTimeObj.centiseconds = rawCurrentTimeObj.centiseconds
-    clearInterval(interval)
-}
+const stopTimer = () => {
+    unformattedPausedTime.centiseconds = unformattedCurrentTime.centiseconds;
+    clearInterval(interval);
+};
 
+//TD captialize Stop and start in css
 //Handle startStop Button Function
-function startStop(){
-    startStopButton = document.getElementById('startStop')
-    startStoptext = startStopButton.innerText
-    resetLapButton = document.getElementById('resetLap')
-    if (startStoptext ==="Stop"){
-        startStopButton.className="start"
-        startStopButton.innerText = "Start"
-        resetLapButton.innerText = "Reset"
-        stopTimer()
-    }
-    else if(startStoptext === "Start"){
-        startStopButton.className="stop"
-        startStopButton.innerText = "Stop"
-        resetLapButton.innerText = "Lap"
+const startStop = () => {
+    const start = "Start";
+    const stop = "Stop";
+    startStopButton = document.getElementById("startStop");
+    startStoptext = startStopButton.innerText;
+    resetLapButton = document.getElementById("resetLap");
+    if (startStoptext === "Stop") {
+        startStopButton.className = "start";
+        startStopButton.innerText = "Start";
+        resetLapButton.innerText = "Reset";
+        stopTimer();
+    } else if (startStoptext === "Start") {
+        startStopButton.className = "stop";
+        startStopButton.innerText = "Stop";
+        resetLapButton.innerText = "Lap";
         timeCounter();
-    } 
-}
+        initiateLiveTimeTable();
+    }
+};
 
 //Handle resetLap Button Function
-function resetLap(){
-    resetLapButton = document.getElementById('resetLap')
-    resetLaptext = resetLapButton.innerText
-    if (resetLaptext ==="Reset"){
-        resetValues()
+const resetLap = () => {
+    resetLapButton = document.getElementById("resetLap");
+    resetLaptext = resetLapButton.innerText;
+    if (resetLaptext === "Reset") {
+        resetValues();
+    } else if (resetLaptext === "Lap") {
+        lapTracker();
     }
-    else if(resetLaptext === "Lap"){
-        lapTracker()
-    } 
-}
+};
 
 //Reset values is called by Reset Lap if the innerhtml says "Reset specifically"
 //This will reset minutes seconds and centiseconds to 0 deletes all laps in current app with a for loop
-function resetValues(){
-    rawCurrentTimeObj.centiseconds = 0;
-    rawPausedTimeObj.centiseconds = 0;
-    fastest = 0;
-    slowest = 0;
-    whichLap = 0 
-    let current_list = document.querySelectorAll("tr")
-    for (let i = 0; (tr = current_list[i]); i++) {
+const resetValues = () => {
+    unformattedCurrentTime.centiseconds = 0;
+    unformattedPausedTime.centiseconds = 0;
+    fastestLapElementAndTime.fastestTime = 0;
+    slowestLapElementAndTime.slowestTime = 0;
+    whichLap = 0;
+    liveTime = false;
+    let currentList = document.querySelectorAll("tr");
+    for (let i = 0; (tr = currentList[i]); i++) {
         tr.parentNode.removeChild(tr);
     }
-    document.getElementById("timer").innerHTML = '00:00.00'
-}
+    document.getElementById("timer").innerText = "00:00.00";
+};
 
 //this function is responsible for adding the html with the laps time and keeping track of the whichLap variable
 // It calls "lapCalc" to begin calculations got laps
-function lapTracker(){
+const initiateLiveTimeTable = () => {
+    if (liveTime === false) {
+        lapTracker();
+    }
+    liveTime = true;
+};
+const lapTracker = () => {
     //Setting tr Information
-    let tr = document.createElement('tr');
-    tr.setAttribute('class','lapsTableRow');
-    tr.setAttribute('id',whichLap);
+    let tableRowLapData = document.createElement("tr");
+    tableRowLapData.setAttribute("class", "lapsTableRow");
+    tableRowLapData.setAttribute("id", whichLap);
 
     //Setting tdLap Information
-    let tdLap = document.createElement('td');
-    tdLap.setAttribute('class','tdLap')
-    tdLap.innerHTML = (`Lap ${whichLap + 1}`)
-    
-    //Setting tdTime Information
-    let tdTime = document.createElement('td');
-    tdTime.setAttribute('class','tdTime')
-    tdTime.innerHTML = (lapCalc(rawCurrentTimeObj.centiseconds))
+    let tdLap = document.createElement("td");
+    tdLap.setAttribute("class", "tdLap");
+    tdLap.innerText = `Lap ${whichLap + 1}`;
 
-    if(whichLap == 0){
+    //Setting tdTime Information
+    let tdTime = document.createElement("td");
+    tdTime.setAttribute("class", "tdTime");
+    tdTime.innerText = lapCalc();
+
+    if (whichLap == 0) {
         //Applying table row
-        document.querySelector('table').appendChild(tr);
-    }else{
-    //Applying table row
-    document.getElementById(whichLap - 1).insertAdjacentElement('beforebegin',tr)
+        document.querySelector("table").appendChild(tableRowLapData);
+    } else {
+        //Applying table row
+        document
+            .getElementById(whichLap - 1)
+            .insertAdjacentElement("beforebegin", tableRowLapData);
     }
     //Applying tdLap Information
-    tr.appendChild(tdLap);
+    tableRowLapData.appendChild(tdLap);
 
-     //Applying tdLap Information
-    tr.appendChild(tdTime);
+    //Applying tdLap Information
+    tableRowLapData.appendChild(tdTime);
+
     //Add to whichLap for tracking
-    if (whichLap == 2){
-        findSlowAndFast()
-    }else if(whichLap > 2){
-        findSlowAndFast()
+    if (whichLap == 2) {
+        findSlowAndFast();
+    } else if (whichLap > 2) {
+        findSlowAndFast();
     }
-    whichLap++
-}
+    whichLap++;
+};
 
 //Function to Save last value of time (in centiseconds) and subtract to newest time in centiseconds to get elapsed laptime
 //This function also converts and replaces the "old" time with the new time after conversion
-function lapCalc(){
-    if(whichLap == 0){
-        lastLapTime = rawCurrentTimeObj.centiseconds
-        displayTime = lastLapTime
-    }else{
-        newLaptime = rawCurrentTimeObj.centiseconds
-        displayTime = (newLaptime - lastLapTime)
-        lastLapTime = newLaptime
-    }
-    convertedBackdisplayTime = convertBack(displayTime)
-    time = LapFormatter(convertedCurrentTimeObj.minutes,convertedCurrentTimeObj.seconds,convertedCurrentTimeObj.centiseconds)
-    return(time)
-}
 
-//Function to convert all values down to centiseconds
-function conversion(minutes,seconds,microseconds){
-    let minutesToSeconds = minutes * 60;
-    let secondsToMicroseconds = (seconds + minutesToSeconds) * 100;
-    let convertedMicroseconds = secondsToMicroseconds + microseconds;
-    return convertedMicroseconds
-}
+const lapCalc = () => {
+    let displayTime = 0;
+    if (whichLap == 0) {
+        lastLapTime = unformattedCurrentTime.centiseconds;
+        displayTime = lastLapTime;
+    } else {
+        displayTime = unformattedCurrentTime.centiseconds - lastLapTime;
+        lastLapTime = unformattedCurrentTime.centiseconds;
+    }
+    formatTime(displayTime);
+    return getFormattedTime();
+};
 
 //Function to convert back to minutes and seconds and centiseconds
-function convertBack(convertedTime){
-    convertedCurrentTimeObj.minutes = Math.floor(convertedTime / 60000)
-        convertedTime -= convertedCurrentTimeObj.minutes * 60000;
-    convertedCurrentTimeObj.seconds = Math.floor(convertedTime / 1000);
-        convertedTime -= convertedCurrentTimeObj.seconds * 1000;
-    convertedCurrentTimeObj.centiseconds = Math.floor((convertedTime)/10);
-}
+const formatTime = (convertedTime) => {
+    formattedCurrentTime.minutes = Math.floor(convertedTime / 60000);
+    convertedTime -= formattedCurrentTime.minutes * 60000;
+    formattedCurrentTime.seconds = Math.floor(convertedTime / 1000);
+    convertedTime -= formattedCurrentTime.seconds * 1000;
+    formattedCurrentTime.centiseconds = Math.floor(convertedTime / 10);
+};
+//Format time from Centiseconds to Minutes, Seconds and Centiseconds
+const getFormattedTime = () => {
+    let formattedCentiseconds = formattedCurrentTime.centiseconds
+        .toString()
+        .padStart(2, "0");
+    let formattedSeconds = formattedCurrentTime.seconds
+        .toString()
+        .padStart(2, "0");
+    let formattedMinutes = formattedCurrentTime.minutes
+        .toString()
+        .padStart(2, "0");
 
-//Function to Format Stopwatch time
-function Formatter(minutes,seconds,microseconds){
-    let formattedMicroseconds = microseconds.toString().padStart(2,"0")
-    let formattedSeconds = seconds.toString().padStart(2,"0")
-    let formattedMinutes = minutes.toString().padStart(2,"0")
+    return `${formattedMinutes}:${formattedSeconds}.${formattedCentiseconds}`;
+};
+//
+const getLiveLapTime = (convertedTime) => {
+    formattedLiveLapTime.minutes = Math.floor(
+        (convertedTime - lastLapTime) / 60000
+    );
+    convertedTime -= formattedLiveLapTime.minutes * 60000;
+    formattedLiveLapTime.seconds = Math.floor(
+        (convertedTime - lastLapTime) / 1000
+    );
+    convertedTime -= formattedLiveLapTime.seconds * 1000;
+    formattedLiveLapTime.centiseconds = Math.floor(
+        (convertedTime - lastLapTime) / 10
+    );
+};
+//
+const getFormattedLiveLapTime = () => {
+    let formattedCentiseconds = formattedLiveLapTime.centiseconds
+        .toString()
+        .padStart(2, "0");
+    let formattedSeconds = formattedLiveLapTime.seconds
+        .toString()
+        .padStart(2, "0");
+    let formattedMinutes = formattedLiveLapTime.minutes
+        .toString()
+        .padStart(2, "0");
 
-return(`${formattedMinutes}:${formattedSeconds}.${formattedMicroseconds}`)
-}
-
-//Function to Format Laps 
-function LapFormatter(minutes,seconds,microseconds){
-    let formattedMicroseconds = microseconds.toString().padStart(2,"0")
-    let formattedSeconds = seconds.toString().padStart(2,"0")
-    let formattedMinutes = minutes.toString().padStart(2,"0")
-
-return(`${formattedMinutes}:${formattedSeconds}.${formattedMicroseconds}`)
-}
-
-function findSlowAndFast(){
-    if(whichLap == 2){
-        for(let i = 0; i <= whichLap; i++){
-            let Lap = document.getElementById(i)
-            let speedAsString = (Lap.querySelector('.tdTime')).innerHTML
-            let speed = parseFloat(speedAsString.replace(':','').replace('.',''))
-            if (fastest == 0){
-                fastest = speed
-                Lap.classList.add('lapsTableRowFastest')
-                fastestLap = Lap
+    return `${formattedMinutes}:${formattedSeconds}.${formattedCentiseconds}`;
+};
+const findSlowAndFast = () => {
+    if (whichLap == 3) {
+        for (let i = 0; i <= whichLap; i++) {
+            let Lap = document.getElementById(i);
+            let speedAsString = Lap.querySelector(".tdTime").innerText;
+            let speed = parseFloat(
+                speedAsString.replace(":", "").replace(".", "")
+            );
+            if (fastestLapElementAndTime.fastestTime == 0) {
+                fastestLapElementAndTime.fastestTime = speed;
+                Lap.classList.add("lapsTableRowFastest");
+                fastestLapElementAndTime.fastestLap = Lap;
             }
-            if(fastest > speed){
-                fastest = speed
-                if(fastestLap != Lap){
-                    fastestLap.classList.remove('lapsTableRowFastest')
-                    Lap.classList.add('lapsTableRowFastest')
-                    fastestLap = Lap
+            if (fastestLapElementAndTime.fastestTime > speed) {
+                fastestLapElementAndTime.fastestTime = speed;
+                if (fastestLapElementAndTime.fastestLap != Lap) {
+                    fastestLapElementAndTime.fastestLap.classList.remove(
+                        "lapsTableRowFastest"
+                    );
+                    Lap.classList.add("lapsTableRowFastest");
+                    fastestLapElementAndTime.fastestLap = Lap;
                 }
             }
-            if(slowest ==  0){
-                slowest = speed
-                Lap.classList.add('lapsTableRowSlowest')
-                slowestLap =  Lap
-                
+            if (slowestLapElementAndTime.slowestTime == 0) {
+                slowestLapElementAndTime.slowestTime = speed;
+                Lap.classList.add("lapsTableRowSlowest");
+                slowestLapElementAndTime.slowestLap = Lap;
             }
-            if(speed > slowest){
-                slowest = speed
-                if(slowestLap != Lap){
-                    slowestLap.classList.remove('lapsTableRowSlowest')
-                    Lap.classList.add('lapsTableRowSlowest')
-                    slowestLap = Lap
+            if (speed > slowestLapElementAndTime.slowestTime) {
+                slowestLapElementAndTime.slowestTime = speed;
+                if (slowestLapElementAndTime.slowestLap != Lap) {
+                    slowestLapElementAndTime.slowestLap.classList.remove(
+                        "lapsTableRowSlowest"
+                    );
+                    Lap.classList.add("lapsTableRowSlowest");
+                    slowestLapElementAndTime.slowestLap = Lap;
                 }
-                
-            }
-        }  
-    }else{
-        let Lap = document.getElementById(whichLap)
-        let speedAsString = (Lap.querySelector('.tdTime')).innerHTML
-        let speed = parseFloat(speedAsString.replace(':','').replace('.',''))
-        if(fastest > speed){
-            fastest = speed
-            if(fastestLap != Lap){
-                fastestLap.classList.remove('lapsTableRowFastest')
-                Lap.classList.add('lapsTableRowFastest')
-                fastestLap = Lap
             }
         }
-        if(speed > slowest){
-            slowest = speed
-            if(slowestLap != Lap){
-                slowestLap.classList.remove('lapsTableRowSlowest')
-                Lap.classList.add('lapsTableRowSlowest')
-                slowestLap = Lap
+    } else if (whichLap > 3) {
+        let Lap = document.getElementById(whichLap - 1);
+        let speedAsString = Lap.querySelector(".tdTime").innerText;
+        let speed = parseFloat(speedAsString.replace(":", "").replace(".", ""));
+        if (fastestLapElementAndTime.fastestTime > speed) {
+            fastestLapElementAndTime.fastestTime = speed;
+            if (fastestLapElementAndTime.fastestLap != Lap) {
+                fastestLapElementAndTime.fastestLap.classList.remove(
+                    "lapsTableRowFastest"
+                );
+                Lap.classList.add("lapsTableRowFastest");
+                fastestLapElementAndTime.fastestLap = Lap;
             }
-            
+        }
+        if (speed > slowestLapElementAndTime.slowestTime) {
+            slowestLapElementAndTime.slowestTime = speed;
+            if (slowestLapElementAndTime.slowestLap != Lap) {
+                slowestLapElementAndTime.slowestLap.classList.remove(
+                    "lapsTableRowSlowest"
+                );
+                Lap.classList.add("lapsTableRowSlowest");
+                slowestLapElementAndTime.slowestLap = Lap;
+            }
         }
     }
-}
+};
